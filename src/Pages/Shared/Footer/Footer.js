71df4e09913793d92e864041727d09e8c5076648
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-12 01:16:41 +0600

    added footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import footer from '../../../assets/images/footer.png'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
-        </div>
+        <footer className="footer p-10"
+        style={{
+            background: `url(${footer})`
+        }}
+        >
+            <div>
+                <span className="footer-title text-accent">Services</span>
+                <Link to="/" className="link link-hover text-accent">Branding</Link>
+                <Link to="/" className="link link-hover text-accent">Design</Link>
+                <Link to="/" className="link link-hover text-accent">Marketing</Link>
+                <Link to="/" className="link link-hover text-accent">Advertisement</Link>
+            </div>
+            <div>
+                <span className="footer-title text-accent">Company</span>
+                <Link to="/" className="link link-hover text-accent">About us</Link>
+                <Link to="/" className="link link-hover text-accent">Contact</Link>
+                <Link to="/" className="link link-hover text-accent">Jobs</Link>
+                <Link to="/" className="link link-hover text-accent">Press kit</Link>
+            </div>
+            <div>
+                <span className="footer-title text-accent">Legal</span>
+                <Link to="/" className="link link-hover text-accent">Terms of use</Link>
+                <Link to="/" className="link link-hover text-accent">Privacy policy</Link>
+                <Link to="/" className="link link-hover text-accent">Cookie policy</Link>
+            </div>
+        </footer>
     );
 };
 
